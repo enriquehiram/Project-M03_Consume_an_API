1958f89abc74fdeb3052546e99dc2812aa316a15
Enrique De la Cruz <delacruz.g.enrique@gmail.com>
2023-02-26 12:07:15 -0600

    primer salvado, todo funcional

---
+++ script.js
@@ -0,0 +1,37 @@
+function getCharacters (done){
+    
+    const results = fetch("https://rickandmortyapi.com/api/character");
+
+    results
+        .then (response => response.json())
+        .then (data => {
+            done(data)
+            log
+        });
+        //.catch((error)=> {
+        //    console.error(error);
+        //});
+}
+
+getCharacters(data => {
+
+    data.results.forEach(personaje => {
+        
+        const article = document.createRange().createContextualFragment(/*html*/`
+        
+        <article>
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+
+            <h2>${personaje.name}</h2>
+            <p>${personaje.status}</p>
+        </article>  
+        
+       `);
+        const main = document.querySelector("main");
+        main.append(article);
+
+    });
+
+});
